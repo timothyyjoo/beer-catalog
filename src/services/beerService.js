d0fbc3f6334732431a0870b7ee25bd4950a14dc2
Tyjoo26 <Tyjoo21@gmail.com>
2019-02-05 17:31:11 -0600

    Extract respone handler logic to separate responseHandlers.js file

---
+++ src/services/beerService.js
@@ -0,0 +1,20 @@
+import { handleResponse, errorLog } from './responseHandlers'
+
+class BeerService {
+  constructor() {
+    this.baseURL = "https://api.punkapi.com/v2/"
+    this.beers = {}
+  }
+  getBeers () {
+    fetch(this.baseURL + "beers")
+      .then(handleResponse)
+      .then((beers) => (this.addBeers(beers)))
+      .catch(errorLog)
+  }
+
+  addBeers (data) {
+    this.beers = Object.assign(this.beers, data)
+  }
+}
+
+export default BeerService;
---
+++ src/services/responseHandlers.js
@@ -0,0 +1,20 @@
+
+export const handleResponse = (response) => {
+  return response.json()
+    .then((json) => {
+      if (!response.ok) {
+        const error = {
+          status: response.status,
+          statusText: response.statusText,
+          json
+        }
+        return Promise.reject(error)
+      }
+      return json
+    }
+  )
+};
+
+export const errorLog = (error) => {
+  console.error({error})
+};
